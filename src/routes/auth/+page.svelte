eee315448945b19d5f40a835073966c669bc7184
Michael Knowles <mike@asaiu.com>
2023-01-22 19:46:00 -0800

    Auth page form initalization

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome banner</h1>
+<form>
+    <label for="username">Username</label>
+    <input type="username" name="username">
+    <label for="password">Password</label>
+    <input type="password" name="password">
+</form>
+<button on:click={null}>Submit</button>
+<h4>sample</h4>
